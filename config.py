9bbfbcdc2f2aa436979a1acfa637fe26020c7dbb
jantillidie <82001155+jantillidie@users.noreply.github.com>
2026-07-02 09:55:28 +0200

    Update config

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 WP_API_URL = os.environ["WP_API_URL"]
 WP_USERNAME = os.environ["WP_USERNAME"] # Username, not App name!
 WP_PASSWORD = os.environ["WP_PASSWORD"]  # App PW
-AI_API_KEY = os.environ["AI_API_KEY"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
-if not all([WP_API_URL, WP_USERNAME, WP_PASSWORD, AI_API_KEY]):
+if not all([WP_API_URL, WP_USERNAME, WP_PASSWORD, OPENAI_API_KEY]):
     raise ValueError("Fehlende Variablen in der .env!")
