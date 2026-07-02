034479b5d3458d664ba25ca91853e463f3b7be14
jantillidie <82001155+jantillidie@users.noreply.github.com>
2026-07-02 12:29:55 +0200

    Update config

--- config.py
+++ config.py
@@ -3,10 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-WP_API_URL = os.environ["WP_API_URL"]
-WP_USERNAME = os.environ["WP_USERNAME"] # Username, not App name!
-WP_PASSWORD = os.environ["WP_PASSWORD"]  # App PW
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+WP_API_URL = os.environ.get("WP_API_URL")
+WP_USERNAME = os.environ.get("WP_USERNAME")  # Username, not App name!
+WP_PASSWORD = os.environ.get("WP_PASSWORD")  # App PW
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 if not all([WP_API_URL, WP_USERNAME, WP_PASSWORD, OPENAI_API_KEY]):
-    raise ValueError("Fehlende Variablen in der .env!")
+    raise ValueError(
+        "Fehlende Variablen in der .env oder .envrc"
+        "Benötigt: WP_API_URL, WP_USERNAME, WP_PASSWORD, OPENAI_API_KEY"
+    )
