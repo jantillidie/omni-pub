3eb14da2ceaea354e575123f814b4d7c9ac1b5bb
jantillidie <82001155+jantillidie@users.noreply.github.com>
2026-07-07 10:56:42 +0200

    Update envs, config and requirements for bluesky and mastodon

--- config.py
+++ config.py
@@ -11,10 +11,14 @@ WP_API_URL = os.environ.get("WP_API_URL")
 WP_USERNAME = os.environ.get("WP_USERNAME")  # Username, not App name!
 WP_PASSWORD = os.environ.get("WP_PASSWORD")  # App PW
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+BLUESKY_HANDLE = os.environ.get("BLUESKY_HANDLE")
+MASTODON_BASE_URL = os.environ.get("MASTODON_BASE_URL")
+BLUESKY_APP_PASSWORD = os.environ.get("BLUESKY_APP_PASSWORD")
+MASTODON_ACCESS_TOKEN = os.environ.get("MASTODON_ACCESS_TOKEN")
 
-if not all([WP_API_URL, WP_USERNAME, WP_PASSWORD, OPENAI_API_KEY]):
+if not all([WP_API_URL, WP_USERNAME, WP_PASSWORD, OPENAI_API_KEY, BLUESKY_HANDLE, MASTODON_BASE_URL, BLUESKY_APP_PASSWORD, MASTODON_ACCESS_TOKEN]):
     raise ValueError(
         "Fehlende Umgebungsvariablen! "
-        "Benötigt: WP_API_URL, WP_USERNAME, WP_PASSWORD, OPENAI_API_KEY. "
+        "Benötigt: WP_API_URL, WP_USERNAME, WP_PASSWORD, OPENAI_API_KEY, BLUESKY_HANDLE, MASTODON_BASE_URL, BLUESKY_APP_PASSWORD, MASTODON_ACCESS_TOKEN "
         "Lege eine .env-Datei an oder nutze .envrc / direnv."
     )
